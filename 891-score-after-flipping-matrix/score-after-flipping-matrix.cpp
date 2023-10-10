058c40d342f84a1125ecb95cee2df2dca054d689
Arush Raghav <arushraghav622@gmail.com>
2023-10-10 17:52:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (49.76%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -1,50 +1,51 @@
 class Solution {
 public:
-    void fliprow(int &r,vector<vector<int>>& grid) //function to toggle row
+    int matrixScore(vector<vector<int>>& grid) 
     {
-        for(int i=0; i<grid[0].size(); i++){
-            grid[r][i]=1-grid[r][i];
-        }
-    }
+        int rows=grid.size();
+        int cols=grid[0].size();
 
-    void flipcol(int &c,vector<vector<int>>& grid){ //function to toggle column
-        for(int j=0; j<grid.size(); j++){
-            grid[j][c]=1-grid[j][c];
-        }
-    }
-
-    int matrixScore(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-//Making the MSB to 1, all values in column 0 should be 1 to get max value
-        for(int i=0; i<n; i++){
-            if(grid[i][0]==0){    
-                fliprow(i,grid);
-            }
-        }
-//traversing and checking which colums have more zeroes than 1, toggling them too
-        for(int j=0; j<m; j++){
-            int cnt0=0;
-            int cnt1=0;
-            for(int i=0; i<n; i++){
-                if(grid[i][j]==0) cnt0++;
-                else cnt1++;
-            }
-            if(cnt0>cnt1){
-                flipcol(j,grid);
-            }
-        }
-//finally calculating the ans
-        int sum=0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==1){
-                    sum+= (1<<(m-j-1));
+        // MAKING THE FIRST COLUMN ALL 1's
+        for(int i=0;i<rows;i++)
+        {
+            if(grid[i][0]==0)
+            {
+                for(int j=0;j<cols;j++)
+                {
+                    if(grid[i][j]==0) grid[i][j]=1;
+                    else grid[i][j]=0;
                 }
             }
         }
-        return sum;
-        
-
+        // FLIP THE COLUMNS WHERE noz>noo
+        for(int j=0;j<cols;j++)
+        {
+            int noz=0;
+            int noo=0;
+            for(int i=0;i<rows;i++)
+            {
+                if(grid[i][j]==0) noz++;
+                else noo++;
+            }
+            if(noz>noo)
+            {
+                for(int i=0;i<rows;i++)
+                {
+                    if(grid[i][j]==0) grid[i][j]=1;
+                    else grid[i][j]=0;
+                }
+            }
+        }
+        int sum=0;
+        for(int i=0;i<rows;i++)
+        {
+            int x=1;
+            for(int j=cols-1;j>=0;j--)
+            {
+                sum+=grid[i][j]*x;
+                x*=2;
+            }
+        }
+        return sum ;
     }
 };

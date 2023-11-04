286dafc0cfba323eae6f5ead142e377e87a42c4f
Arush Raghav <arushraghav622@gmail.com>
2023-11-05 01:10:42 +0530

    Time: 498 ms (21.98%) | Memory: 23.3 MB (35.65%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-    void prin(vector<vector<bool>>& mat)
-    {
-        for(int i = 0; i<mat.size() ; i++)
-        {
-            for(int j = 0; j<mat.size(); j++)
-            {
-                cout<<mat[i][j]<<" ";
-            }
-            cout<<"\n";
-        }cout<<"aa\n";
-    }
     string longestPalindrome(string s) 
     {
         vector<vector<bool>> data(s.size(),vector<bool>(s.size(),false));
@@ -20,14 +9,14 @@ public:
         {
             for(int j = 0 ; j+i<=s.size() ; j++)
             {
-                if(i>2 && (s.substr(j,1) == s.substr(j+i-1,1) )&& data[j+1][j+i-2] )
+                if(i>2 && s[j] == s[j+i-1] && data[j+1][j+i-2] )
                 {
                     data[j][j+i-1] = true;
                     data[j+i-1][j] = true;
                     b = j;
                     len = i;
                 }
-                if(s.substr(j,1) == s.substr(j+i-1,1) && i == 2)
+                if(s[j] == s[j+i-1] && i == 2)
                 {
                     data[j][j+i-1] = true;
                     data[j+i-1][j] = true;

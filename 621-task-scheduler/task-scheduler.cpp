6e77e555d53759bb62e4859427dd98f61f834538
Arush Raghav <arushraghav622@gmail.com>
2024-03-19 13:54:51 +0530

    Time: 51 ms (80.04%) | Memory: 38 MB (80.77%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        // Create a frequency array to keep track of the count of each task
-        vector<int> freq(26);
+        // Counter array to store the frequency of each task
+        int counter[26] = {0};
+        int maximum = 0;
+        int maxCount = 0;
+        // Traverse through tasks to calculate task frequencies
         for (char task : tasks) {
-            freq[task - 'A']++;
+            counter[task - 'A']++;
+            if (maximum == counter[task - 'A']) {
+                maxCount++;
+            }
+            else if (maximum < counter[task - 'A']) {
+                maximum = counter[task - 'A'];
+                maxCount = 1;
+            }
         }
-
-        // Sort the frequency array in non-decreasing order
-        sort(freq.begin(), freq.end());
-        // Calculate the maximum frequency of any task
-        int maxFreq = freq[25] - 1;
-        // Calculate the number of idle slots that will be required
-        int idleSlots = maxFreq * n;
-
-        // Iterate over the frequency array from the second highest frequency to the lowest frequency
-        for (int i = 24; i >= 0 && freq[i] > 0; i--) {
-            // Subtract the minimum of the maximum frequency and the current frequency from the idle slots
-            idleSlots -= min(maxFreq, freq[i]);
-        }
-
-        // If there are any idle slots left, add them to the total number of tasks
-        return idleSlots > 0 ? idleSlots + tasks.size() : tasks.size();
+        
+        // Calculate idle slots, available tasks, and idles needed
+        int partCount = maximum - 1;
+        int partLength = n - (maxCount - 1);
+        int emptySlots = partCount * partLength;
+        int availableTasks = tasks.size() - maximum * maxCount;
+        int idles = max(0, emptySlots - availableTasks);
+        
+        // Return the total time required
+        return tasks.size() + idles;
     }
 };
